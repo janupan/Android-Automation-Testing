03146c7cf3f3856dbd5956f0208f8086c81ce828
januaripanjaitan <januaripanjaitan@192.168.1.190>
2018-07-16 15:09:54 +0700

    Update Report using gallery picture and test suite

--- Scripts/Upload Post (Report)/Report Using Gallery Picture/TC3_Invalid_Post(Empty Field)/Script1531724514722.groovy
+++ Scripts/Upload Post (Report)/Report Using Gallery Picture/TC3_Invalid_Post(Empty Field)/Script1531724514722.groovy
@@ -19,7 +19,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-Mobile.startApplication('F:\\QLUE INTERNSHIP\\app-dev-debug.apk', true)
+Mobile.startApplication('/Users/januaripanjaitan/git/Android-Automation-Testing/Android Apk/Polisi110-v0112-staging-11072018.apk', true)
 
 Mobile.setText(findTestObject('android.widget.EditText0 - Email (128)'), 'fajar2@mailinator.com', 0)
 
---
+++ Test Suites/Upload Post (Report)/Report using Gallery Picture.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Report using Gallery Picture</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>038471c4-69b3-4b81-902e-0d453790c8cd</testSuiteGuid>
+   <testCaseLink>
+      <guid>e3fd3aff-0f86-45cb-a956-856f13b9a52d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Upload Post (Report)/Report Using Gallery Picture/TC1_Valid_Post</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c307ffa5-5ba0-4cd0-bcb5-089b92fb1773</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Upload Post (Report)/Report Using Gallery Picture/TC2_Invalid_Gallery_Picture_Location_Tag</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c82d041c-c28c-4b00-b0f6-ca9dcb99151b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Upload Post (Report)/Report Using Gallery Picture/TC3_Invalid_Post(Empty Field)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>010409f6-db5f-4737-ad5b-be735560259a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Upload Post (Report)/Report Using Gallery Picture/TC4_Invalid_Post(has not report detail)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8251ecde-62b6-435c-8446-a0093fd043e0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Upload Post (Report)/Report Using Gallery Picture/TC5_Invalid_Post(has not location detail)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0a28bd67-50d0-468d-93c7-5cc3b74114c9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Upload Post (Report)/Report Using Gallery Picture/TC6_Invalid_Post(has not label)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a4516e03-90b3-47de-b29a-95fda5bd2863</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Upload Post (Report)/Report Using Gallery Picture/TC7_Invalid_Post_Picture(No Connection)</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
